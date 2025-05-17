c07e7901a74ba81d29d3d3ce0f3154ff3ca61e5b
Samuel <samuel.hu.152002@gmail.com>
2025-05-16 23:50:38 -0500

    Proyecto_Prueba

---
+++ Hola_Mundo.py
@@ -0,0 +1,34 @@
+print("Hola mundo")
+print("Prueba para repositorio")
+
+#Suma
+
+numero1 =24
+numero2 =8
+resultado = numero1+numero2
+
+print("La suma del numero1 y numero2 es", resultado)
+
+#Resta
+
+numero1 =24
+numero2 =8
+resultado = numero1-numero2
+
+print("La resta del numero1 y numero2 es", resultado)
+
+#Multiplicación
+
+numero1 =24
+numero2 =8
+resultado = numero1*numero2
+
+print("La multiplicación del numero1 y numero2 es", resultado)
+
+#División
+
+numero1 =24
+numero2 =8
+resultado = numero1/numero2
+
+print("La divición del numero1 y numero2 es", resultado)
